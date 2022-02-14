d766bb3d7dfbdf7fd50822a46a61b7d5e4eae026
Lauren McCarey <lrm2@williams.edu>
2022-02-14 17:45:48 -0500

    have lists of professor info separated

--- parse18.py
+++ parse18.py
@@ -1,21 +1,19 @@
-import pdfplumber 
+# import pdfplumber 
 
-text = ''
-for i in range(62, 90): 
-# for i in range(62, 64): 
-    with pdfplumber.open('/Users/laurenmccarey/math308/catalog_2018-19.pdf') as temp: 
-        page = temp.pages[i]
-        text = page.extract_text()
-    with open ('catalog_2018-19.txt', 'a') as f: 
-        f.write(text)
+# text = ''
+# for i in range(62, 90): 
+#     with pdfplumber.open('/Users/laurenmccarey/math308/catalog_2018-19.pdf') as temp: 
+#         page = temp.pages[i]
+#         text = page.extract_text()
+#     with open ('catalog_2018-19.txt', 'a') as f: 
+#         f.write(text)
 
-# with open ('catalog_2018-19.txt', 'w') as f: 
-#      f.write(corpus)
+lines = []
+with open ('catalog_2018-19.txt') as f: 
+    lines = f.readlines()
 
-# lines = []
-# with open ('catalog_2018-19.txt') as f: 
-#     lines = f.readlines()
-
-# for line in lines: 
-#     print('\n')
-#     print(line.split(',', maxsplit=2))
+for line in lines: 
+    print('\n')
+    line = line.split(',', maxsplit=2)
+    line = [item.strip(' \n') for item in line]
+    print(line)
