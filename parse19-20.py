ecb47286b25eef6c8361130f246146229ac6121e
Lauren McCarey <lrm2@williams.edu>
2022-02-15 15:50:21 -0500

    Tuesday 3:50

---
+++ parse19-20.py
@@ -0,0 +1,42 @@
+import pdfplumber 
+import pandas as pd
+import csv
+
+text = ''
+for i in range(65, 96): 
+    with pdfplumber.open('/Users/laurenmccarey/math308/catalog_2019-20.pdf') as temp: 
+        page = temp.pages[i]
+        text = page.extract_text()
+    with open ('catalog_2019-20.txt', 'a') as f: 
+        f.write(text)
+
+# lines = []
+# with open ('catalog_2019-20.txt') as f: 
+#     lines = f.readlines()
+
+# for line in lines: 
+#     # print('\n')
+#     line = line.split(',', maxsplit=2)
+#     line = [item.strip(' \n') for item in line]
+
+#     if (len(line) == 3): 
+#         if ("on leave" in line[2]): 
+#             if ("on leave Fall 2018" in line[2]): 
+#                 line.append('f') 
+#                 line[2] = line[2].replace(", on leave Fall 2018", '')
+#             if ("on leave Spring 2019" in line[2]): 
+#                 line.append('s')
+#                 line[2] = line[2].replace(", on leave Spring 2019", '')
+#             if ("on leave 2018-2019" in line[2]): 
+#                 line.append('y')
+#                 line[2] = line[2].replace(", on leave 2018-2019", '') 
+#             print(line)
+#         else: 
+#             line.append('n')
+#     if (len(line) == 2): 
+#         line.append('none')
+#         line.append('n')
+#     if (len(line) == 1): 
+#         line.append("none")
+#         line.append("none")
+#         line.append('n')
