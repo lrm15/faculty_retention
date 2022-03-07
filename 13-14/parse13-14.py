0dd87b32ffdcb5870499c21ba499a65c382687c3
Lauren McCarey <lrm2@williams.edu>
2022-03-07 17:16:20 -0500

    started parsing 13-14

---
+++ 13-14/parse13-14.py
@@ -0,0 +1,18 @@
+import pdfplumber 
+import pandas as pd
+import csv
+from pathlib import Path 
+
+# text = ''
+# for i in range(336, 344): 
+#     with pdfplumber.open('/Users/laurenmccarey/math308/13-14/catalog_2013-14.pdf') as temp: 
+#         page = temp.pages[i]
+#         text = page.extract_text()
+#     with open ('catalog_2013-14.txt', 'a') as f: 
+#         f.write(text)
+
+# * On leave 2013-2014
+# ** On leave first semester 
+# ***On leave second semester 
+# + Visitor first semester 
+# ++ Visitor second semester
