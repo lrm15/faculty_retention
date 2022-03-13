0b4c7322084fed2ecd9bafa8d0c9d6dc80a50a3a
Lauren McCarey <lrm2@williams.edu>
2022-03-13 19:42:55 -0400

    making good progress

--- 21-22/parse21-22.py
+++ 21-22/parse21-22.py
@@ -16,4 +16,56 @@ lines = []
 with open ('catalog_2021-22.txt') as f: 
     lines = f.readlines()
 
-print(lines)
+cleaned = []
+for line in lines: 
+
+    if ("Women, Gender & Sexuality Stdy" in line): 
+        line = line.replace("Women, Gender & Sexuality Stdy", "WGGS")
+
+    elif ("Women's, Gender and Sexuality Studies" in line): 
+        line = line.replace("Women's, Gender and Sexuality Studies", "WGGS")
+
+    elif ("Women's, Gender, and Sexuality Studies" in line): 
+        line = line.replace("Women's, Gender, and Sexuality Studies", "WGGS")
+
+    elif (", Jr." in line): 
+        line = line.replace(", Jr.", " Jr.")
+    
+    line = line.split(";", maxsplit=1)
+
+    if (len(line) > 1): 
+        line[1] = line[1].strip()
+    else: 
+        line.append(None)
+
+    line[0] = line[0].split(",", maxsplit=1)
+    line[0] = [item.strip() for item in line[0]]
+
+    print(line)
+
+    # line = line.split(',', maxsplit=2)
+    # line = [item.strip(' \n') for item in line]
+
+
+#     if (len(line) == 3): 
+#         if ("on leave" in line[2]): 
+#             if ("on leave Fall 2020" in line[2]): 
+#                 line.append('f') 
+#                 line[2] = line[2].replace(", on leave Fall 2020", '')
+#             if ("on leave Spring 2021" in line[2]): 
+#                 line.append('s')
+#                 line[2] = line[2].replace(";, on leave Spring 2021", '')
+#             if ("on leave 2020-2021" in line[2]): 
+#                 line.append('y')
+#                 line[2] = line[2].replace(", on leave 2020-2021", '') 
+#             # print(line)
+#         else: 
+#             line.append('n')
+#     if (len(line) == 2): 
+#         line.append('none')
+#         line.append('n')
+#     if (len(line) == 1): 
+#         line.append("none")
+#         line.append("none")
+#         line.append('n')
+#     cleaned.append(line)
