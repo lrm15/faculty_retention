18f1ba2797083f97f9c73c7ec1e1fb73e96e9197
Lauren McCarey <lrm2@williams.edu>
2022-03-14 11:08:30 -0400

    finished parsing 21-22

--- 21-22/parse21-22.py
+++ 21-22/parse21-22.py
@@ -12,60 +12,120 @@ from pathlib import Path
 #         f.write(text)
 #     print(i)
 
-lines = []
-with open ('catalog_2021-22.txt') as f: 
-    lines = f.readlines()
+# lines = []
+# with open ('catalog_2021-22.txt') as f: 
+#     lines = f.readlines()
 
-cleaned = []
-for line in lines: 
+# cleaned = []
+# for line in lines: 
 
-    if ("Women, Gender & Sexuality Stdy" in line): 
-        line = line.replace("Women, Gender & Sexuality Stdy", "WGGS")
+#     line = line.lower()
 
-    elif ("Women's, Gender and Sexuality Studies" in line): 
-        line = line.replace("Women's, Gender and Sexuality Studies", "WGGS")
+#     if ("women, gender & sexuality stdy" in line): 
+#         line = line.replace("women, gender & sexuality stdy", "WGGS")
 
-    elif ("Women's, Gender, and Sexuality Studies" in line): 
-        line = line.replace("Women's, Gender, and Sexuality Studies", "WGGS")
+#     elif ("women's, gender and sexuality studies" in line): 
+#         line = line.replace("women's, gender and sexuality studies", "WGGS")
 
-    elif (", Jr." in line): 
-        line = line.replace(", Jr.", " Jr.")
+#     elif ("women's, gender, and sexuality studies" in line): 
+#         line = line.replace("women's, gender, and sexuality studies", "WGGS")
+
+#     elif (", jr." in line): 
+#         line = line.replace(", jr.", " jr.")
     
-    line = line.split(";", maxsplit=1)
+#     line = line.split(";", maxsplit=1)
 
-    if (len(line) > 1): 
-        line[1] = line[1].strip()
-    else: 
-        line.append(None)
+#     if (len(line) > 1): 
+#         line[1] = line[1].strip()
+#     else: 
+#         line.append(None)
 
-    line[0] = line[0].split(",", maxsplit=1)
-    line[0] = [item.strip() for item in line[0]]
+#     line[0] = line[0].split(",", maxsplit=1)
+#     line[0] = [item.strip() for item in line[0]]
 
-    print(line)
+#     line[0][0] = line[0][0].split(" ", maxsplit=2)
+#     line[0][0] = [item.strip() for item in line[0][0]]
 
-    # line = line.split(',', maxsplit=2)
-    # line = [item.strip(' \n') for item in line]
+#     if (line[1]): 
+#         if ("on leave fall 2021" in line[1]): 
+#             line.append("f")
+#             line[1] = line[1].replace("; on leave fall 2021", "")
+#         elif ("on leave spring 2022" in line[1]): 
+#             line.append("s")
+#             line[1] = line[1].replace("; on leave spring 2022", "")
+#         elif ("on leave 2021-2022" in line[1]): 
+#             line.append("y")
+#             line[1] = line[1].replace("on leave 2021-2022", "")
+#         else:
+#             line.append("n")
 
+#     else: 
+#         line.append("n") 
+    
+#     if (len(line) < 3): 
+#         print(line)
+#         break
 
-#     if (len(line) == 3): 
-#         if ("on leave" in line[2]): 
-#             if ("on leave Fall 2020" in line[2]): 
-#                 line.append('f') 
-#                 line[2] = line[2].replace(", on leave Fall 2020", '')
-#             if ("on leave Spring 2021" in line[2]): 
-#                 line.append('s')
-#                 line[2] = line[2].replace(";, on leave Spring 2021", '')
-#             if ("on leave 2020-2021" in line[2]): 
-#                 line.append('y')
-#                 line[2] = line[2].replace(", on leave 2020-2021", '') 
-#             # print(line)
-#         else: 
-#             line.append('n')
-#     if (len(line) == 2): 
-#         line.append('none')
-#         line.append('n')
-#     if (len(line) == 1): 
-#         line.append("none")
-#         line.append("none")
-#         line.append('n')
+#     if ("visiting" in line[0][1]): 
+#         line.append(1)
+#     else: 
+#         line.append(0)
+
+        
 #     cleaned.append(line)
+
+# last = []
+# first = []
+# middle = []
+# title = []
+# degrees = []
+# leave = []
+# visiting = []
+
+# for faculty in cleaned: 
+    
+#     if (len(faculty) < 3): 
+#         print(faculty)
+#         break 
+
+#     if (len(faculty[0][0]) == 3): 
+#         last.append(faculty[0][0][0])
+#         middle.append(faculty[0][0][1])
+#         first.append(faculty[0][0][2])
+#     elif (len(faculty[0][0]) == 2): 
+#         last.append(faculty[0][0][0])
+#         middle.append(None)
+#         first.append(faculty[0][0][1])
+    
+#     title.append(faculty[0][1])
+
+#     degrees.append(faculty[1])
+
+#     leave.append(faculty[2])
+
+#     visiting.append(faculty[3])
+
+# # print(len(first))
+# # print(len(last))
+# # print(len(middle))
+# # print(len(title))
+# # print(len(degrees))
+# # print(len(leave))
+# # print(len(visiting))
+
+# df = pd.DataFrame()
+# df['last'] = last 
+# df['first'] = first
+# df['middle'] = middle 
+# df['title'] = title
+# df['degrees'] = degrees 
+# df['leave'] = leave
+# df['visiting'] = visiting 
+
+# # print(df)
+
+# filepath = Path('/Users/laurenmccarey/math308/21-22/faculty21-22.csv')  
+# filepath.parent.mkdir(parents=True, exist_ok=True)  
+# df.to_csv(filepath)  
+
+
